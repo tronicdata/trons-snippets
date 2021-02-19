e97aef3855f37007abcb7d69f19add254e6f87e5
Larry Buffaloboy <larrybuffaloboy@gmail.com>
2021-02-19 10:18:58 -0500

    Create snippet.js
    Added getUrlQuery function

---
+++ snippet.js
@@ -0,0 +1,12 @@
+
+//Looks at URL and splits the query so you can grab values by their corresponding 'key'
+function getUrlQueryVariable(variable) {
+	var query = window.location.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		if (decodeURIComponent(pair[0]) == variable) {
+			return decodeURIComponent(pair[1]);
+		}
+	}
+}
